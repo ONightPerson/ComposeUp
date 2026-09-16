de2485106f9b4cacefa0a6c399aae7df8e1a89ef
liubaozhu <1360323023@qq.com>
2026-09-16 18:00:52 +0800

    优化代码

--- app/src/main/java/com/example/composeup/datastore/data/local/room/ProductDao.kt
+++ app/src/main/java/com/example/composeup/datastore/data/local/room/ProductDao.kt
@@ -1,6 +1,11 @@
 package com.example.composeup.datastore.data.local.room
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import com.example.composeup.datastore.data.model.ProductEntity
 import kotlinx.coroutines.flow.Flow
 
